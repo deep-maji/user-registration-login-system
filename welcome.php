5eab0c8099d2f7559d9cce9f90e2c3aa680dee1b
deep-maji <majideep179@gmail.com>
2025-11-06 21:53:02 +0530

    add validation in welcome.php

--- welcome.php
+++ welcome.php
@@ -12,44 +12,50 @@ if (!isset($_COOKIE['_theme'])) {
 
 $showAlert = false;
 $showError = false;
+$phoneLenError = false;
 if ($_SERVER['REQUEST_METHOD'] == "POST") {
   include "./partials/_dbconnect.php";
   if (isset($_POST['phone'])) {
-    $phone = $_POST['phone'];
-    $id = $_SESSION['id'];
-    $phonesql = "UPDATE `users` SET `phone` = $phone WHERE id = '$id';";
-    $result = mysqli_query($con, $phonesql);
-    if ($result) {
-      $_SESSION['phone'] = $phone;
-      header("location:welcome.php");
+    if (strlen($_POST['phone']) == 10) {
+      $id = $_SESSION['id'];
+      $phone = !empty($_POST['phone']) ? $_POST['phone'] : 'NULL';
+      $phonesql = "UPDATE `users` SET `phone` = $phone WHERE id = '$id'";
+      $result = mysqli_query($con, $phonesql);
+      if ($result) {
+        $_SESSION['phone'] = $phone;
+        header("location:welcome.php");
+      }
+    } else {
+      $phoneLenError = "Phone number must be 10 digits long.";
     }
   } else {
-    $username = $_POST['username'];
-    $email = $_POST['email'];
+    if (($_SESSION['username'] != $_POST['username']) || $_SESSION['email'] != $_POST['email']) {
+      $username = $_POST['username'];
+      $email = $_POST['email'];
 
-    $existsSql = "SELECT `username` FROM `users` WHERE `username` = '$username'";
-    $userName_existResult = mysqli_query($con, $existsSql);
-    $num_of_rows_username = mysqli_num_rows($userName_existResult);
+      $existsSql = "SELECT `username` FROM `users` WHERE `username` = '$username'";
+      $userName_existResult = mysqli_query($con, $existsSql);
+      $num_of_rows_username = mysqli_num_rows($userName_existResult);
 
-    $existsSql = "SELECT `email` FROM `users` WHERE `email` = '$email'";
-    $email_existResult = mysqli_query($con, $existsSql);
-    $num_of_rows_email = mysqli_num_rows($email_existResult);
-    if ($num_of_rows_username > 0 && $num_of_rows_email > 0) {
-      $showError = "Username or email already exists.";
-    } else {
-      $id = $_SESSION['id'];
-      $sql = "UPDATE `users` SET `username` = '$username', `email` = '$email' WHERE id = '$id';";
-      $result = mysqli_query($con, $sql);
-      if ($result) {
-        $showAlert = true;
-        $_SESSION['isLogin'] = true;
-        $_SESSION['username'] = $username;
-        $_SESSION['email'] = $email;
-        header("location:welcome.php");
+      $existsSql = "SELECT `email` FROM `users` WHERE `email` = '$email'";
+      $email_existResult = mysqli_query($con, $existsSql);
+      $num_of_rows_email = mysqli_num_rows($email_existResult);
+      if ($num_of_rows_username > 0 && $num_of_rows_email > 0) {
+        $showError = "Username or email already exists.";
+      } else {
+        $id = $_SESSION['id'];
+        $sql = "UPDATE `users` SET `username` = '$username', `email` = '$email' WHERE id = '$id';";
+        $result = mysqli_query($con, $sql);
+        if ($result) {
+          $showAlert = "Your profile has been updated successfully!";
+          $_SESSION['isLogin'] = true;
+          $_SESSION['username'] = $username;
+          $_SESSION['email'] = $email;
+          // header("location:welcome.php");
+        }
       }
     }
   }
-
 }
 
 
@@ -64,10 +70,17 @@ if ($_SERVER['REQUEST_METHOD'] == "POST") {
   <title>Welcome - <?= $_SESSION['username']; ?></title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet"
     integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
-  <link rel="stylesheet" href="css/style.css">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.1/css/all.min.css"
     integrity="sha512-2SwdPD6INVrV/lHTZbO2nodKhrnDdJK9/kg2XD1r9uGqPo1cUbujc+IYdlYdEErWNu69gVcYgdxlmVmzTWnetw=="
     crossorigin="anonymous" referrerpolicy="no-referrer" />
+  <link rel="stylesheet" href="css/style.css">
+  <style>
+    #phone::-webkit-outer-spin-button,
+    #phone::-webkit-inner-spin-button {
+      -webkit-appearance: none;
+      margin: 0;
+    }
+  </style>
 </head>
 
 <body>
@@ -75,7 +88,7 @@ if ($_SERVER['REQUEST_METHOD'] == "POST") {
   <?php
   if ($showAlert) {
     echo '<div class="d-flex justify-content-center mt-3"><div class="alert alert-success alert-dismissible fade show col-12 col-sm-12 col-md-5" role="alert">
-    <strong>Sucess! Your account is now created and you can login </strong>
+    ' . $showAlert . '
     <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
   </div></div>';
   }
@@ -83,6 +96,13 @@ if ($_SERVER['REQUEST_METHOD'] == "POST") {
   if ($showError) {
     echo '<div class="d-flex justify-content-center mt-3"><div class="alert alert-danger alert-dismissible fade show col-12 col-sm-12 col-md-5" role="alert">
     ' . $showError . '
+    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+  </div></div>';
+  }
+
+  if ($phoneLenError) {
+    echo '<div class="d-flex justify-content-center mt-3"><div class="alert alert-danger alert-dismissible fade show col-12 col-sm-12 col-md-5" role="alert">
+    ' . $phoneLenError . '
     <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
   </div></div>';
   }
@@ -136,19 +156,22 @@ if ($_SERVER['REQUEST_METHOD'] == "POST") {
           </button>
           <div class="collapse" id="updateProfile">
             <div class="my-3"><strong>Update your profile</strong></div>
-            <form action="./welcome.php" method="post">
+            <form action="./welcome.php" method="post" class="needs-validation" novalidate>
               <div class="row mb-3">
                 <label for="username" class="col-sm-2 col-form-label">Username</label>
                 <div class="col-sm-10">
                   <input value="<?= $_SESSION['username'] ?>" type="text" name="username" class="form-control"
-                    id="username">
+                    id="username" required>
+                  <div class="invalid-feedback">username should not be empty</div>
                 </div>
               </div>
 
               <div class="row mb-3">
                 <label for="email" class="col-sm-2 col-form-label">Email</label>
                 <div class="col-sm-10">
-                  <input value="<?= $_SESSION['email'] ?>" type="email" name="email" class="form-control" id="email">
+                  <input value="<?= $_SESSION['email'] ?>" type="email" name="email" class="form-control" id="email"
+                    required>
+                  <div class="invalid-feedback">Email should not be empty</div>
                 </div>
               </div>
 
@@ -160,7 +183,8 @@ if ($_SERVER['REQUEST_METHOD'] == "POST") {
             <label class="col-sm-2 col-form-label">Phone</label>
             <div class="col-sm-10">
               <?php
-              if (!empty($_SESSION['phone'])) {
+              // echo $_SESSION['phone'];
+              if ($_SESSION['phone'] != 'NULL') {
                 echo '<div class="d-flex">
                         <input readonly value="' . $_SESSION["phone"] . '" type="number" class="form-control">
                         <button class="ms-3 btn btn-primary" type="button" data-bs-toggle="collapse" data-bs-target="#addphone"
@@ -178,12 +202,13 @@ if ($_SERVER['REQUEST_METHOD'] == "POST") {
               ?>
 
               <div class="collapse mt-3" id="addphone">
-                <form action="./welcome.php" method="post">
+                <form action="./welcome.php" method="post" class="needs-validation" novalidate>
 
                   <div class="row mb-3">
                     <div class="col-sm-10 d-flex">
                       <input value="<?= $_SESSION['phone'] ?>" type="number" name="phone" class="form-control"
-                        id="phone">
+                        id="phone" required>
+                      <!-- <div class="invalid-feedback">Phone number should 10 digits</div> -->
                       <button type="submit" class="mx-3 btn btn-primary">Save</button>
                     </div>
                   </div>
@@ -199,6 +224,7 @@ if ($_SERVER['REQUEST_METHOD'] == "POST") {
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js"
     integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI"
     crossorigin="anonymous"></script>
+  <script src="js/script.js"></script>
   <script>
     const html = document.documentElement;
     const themeRadios = document.querySelectorAll('input[name="themeRadios"]');
